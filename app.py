a35df17edd62d621e714c7ed05db7a9c796ac749
Bighetto <arthurbighetto36@gmail.com>
2025-07-09 22:34:34 -0300

    adicionado passos de validacao dos dados

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import kagglehub
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 # 1. Baixar o dataset
 path = kagglehub.dataset_download("cherngs/heart-disease-cleveland-uci")
@@ -14,7 +17,19 @@ print(df.head())
 print(df.info())     
 print(df.describe())
 
-print(df['condition'].value_counts())
-print(df.head(10))
+#1 passo: Separar entradas e saidas de dados com base no nosso csv.
+X = df.drop("condition", axis=1)
+y = df["condition"]
 
+#2 passo: criar dados de treino e teste (test_size significa a porcentagem que sera dos dados de teste e o restante de treino. )
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+modelo = LogisticRegression(max_iter=1000)
+modelo.fit(X_train, y_train)
+
+y_pred = modelo.predict(X_test)
+
+print("Acurácia:", accuracy_score(y_test, y_pred))
+
+print(df['condition'].value_counts())
 
